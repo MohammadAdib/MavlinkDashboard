abc4487bc665a68dd008f9f97141039465b7314b
Mohammad Adib <m.a.adib96@gmail.com>
2022-01-29 22:42:27 -0800

    Maps

--- app/src/main/java/mohammad/adib/mavlinkdashboard/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/mohammad/adib/mavlinkdashboard/ui/dashboard/DashboardFragment.kt
@@ -8,9 +8,16 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import mohammad.adib.mavlinkdashboard.R
 import mohammad.adib.mavlinkdashboard.databinding.FragmentDashboardBinding
 
-class DashboardFragment : Fragment() {
+class DashboardFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var homeViewModel: DashboardViewModel
     private var _binding: FragmentDashboardBinding? = null
@@ -37,8 +44,24 @@ class DashboardFragment : Fragment() {
         return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        val sydney = LatLng(-34.0, 151.0)
+        map.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
+        )
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
